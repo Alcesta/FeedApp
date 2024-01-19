226746c046712715579d3b9c5bd56baa963ea808
Alcesta <angrypandaria@gmail.com>
2024-01-19 13:19:34 +0300

    Сверстала экран Входа

---
+++ ImageFeed/Constants.swift
@@ -0,0 +1,7 @@
+import Foundation
+
+let accessKey = "qPGGVlVzYgUdPU9GUwcR6DJSg6SdoiY2T97aYJAcFNU"
+let secretKey = "zVyIbXBArzmjBANdYb-h_VQ0pVrXhjs-UyDbXqNfLfM"
+let redirectURI = "urn:ietf:wg:oauth:2.0:oob"
+let accessScope = "public+read_user+write_likes"
+let defaultBaseURL = URL(string: "https://api.unsplash.com")!
